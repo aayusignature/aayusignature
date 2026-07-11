8b0341db9c424daa0b0809affa579a2430582970
aayusignature <vishalvermadanger@gmail.com>
2026-07-11 20:28:52 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,19 +1,16 @@
 // js/config.js
 const CONFIG = {
-    // YouTube Credentials
+    // YouTube Engine Credentials
     YOUTUBE_API_KEY: 'AIzaSyAkIaFny9YIVkX4Mqhar1x7pGZVZ9dMSbw', 
     CHANNEL_ID: 'UC3WipuhOAkBgPxskQ9Bbstg',
     BASE_URL: 'https://www.googleapis.com/youtube/v3',
 
-    // Social & Contact Info
+    // Social & Connect Links
     SOCIALS: {
         FB: 'https://www.facebook.com/share/1FreoK84JK/',
         INSTA: 'https://www.instagram.com/aayusignature/',
-        EMAIL: 'support@aayusignature.qzz.io',
-        EMAIL_HREF: 'mailto:support@aayusignature.qzz.io'
+        EMAIL: 'support@aayusignature.qzz.io'
     },
 
-    // Brand Settings
-    BRAND_NAME: 'AAYU SIGNATURE'
+    BRAND: 'AAYU SIGNATURE'
 };
-
